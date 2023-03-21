14c2e6f0f10ee53039a9d88dc62111040d6bc3e5
Adrian Fagerland <adrian.fagerland@hotmail.com>
2023-03-21 19:06:04 +0100

    add commit

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 
 import sqlite3
 
+
 def init():
     conn = create_connection("sql/tog.db")
     setup_database(conn)
     return conn
+
 
 def create_connection(db_file):
     conn = None
@@ -16,12 +18,15 @@ def create_connection(db_file):
 
     return conn
 
+
 def execute_sql_file(conn: sqlite3.Connection, sql_file_path):
     with open(sql_file_path, 'r') as file:
         sql_commands = file.read().split(';')
         for command in sql_commands:
             if command.strip():
                 conn.execute(command)
+                conn.commit()
+
 
 def setup_database(conn: sqlite3.Connection):
     with open("sql/lag_tabeller.sql", "r") as file:
@@ -32,10 +37,8 @@ def setup_database(conn: sqlite3.Connection):
         conn.commit()
     except sqlite3.Error as e:
         print("Error executing SQL script:", e)
-    
+
     execute_sql_file(conn, "sql/insert_vogntyper.sql")
     execute_sql_file(conn, "sql/insert_nordlandsbanen.sql")
     execute_sql_file(conn, "sql/insert_togruter.sql")
     execute_sql_file(conn, "sql/insert_togruteforekomster.sql")
-    
-
