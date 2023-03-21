08187737c31a7f26d1749dacacd87d5c82bd556f
Adrian Fagerland <adrian.fagerland@hotmail.com>
2023-03-21 17:18:38 +0100

    f

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Kjør main.py for å kjøre programmet.
+Kjør `python3 main.py` for å kjøre programmet.
 
-Kjør `python3 setup.py` før første gang
+Hvis du bruker windows: kjør `python3 setup.py` før første gang
--- setup.py
+++ setup.py
@@ -3,9 +3,3 @@ import subprocess
 
 if platform.system() == 'Windows':
     subprocess.check_call(['pip3', 'install', 'windows-curses'])
-else:
-    subprocess.check_call(['pip3', 'install', 'curses'])
-
-subprocess.check_call(['pip3', 'install', 'pysqlite3'])
-
-# Rest of your code using curses and sqlite3
