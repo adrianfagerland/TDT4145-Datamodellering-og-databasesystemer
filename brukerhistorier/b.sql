8e7647b7e3808b8187a80092f1b9335ad02246fd
Adrian Fagerland <adrian.fagerland@hotmail.com>
2023-03-25 19:35:45 +0100

    fix tid og mye

--- brukerhistorier/b.sql
+++ brukerhistorier/b.sql
@@ -57,12 +57,12 @@ VALUES ('TrondheimSBodøMorgen', 'SJ', 'ToSitte', TRUE, TRUE, TRUE, TRUE, TRUE, 
 
 INSERT OR IGNORE INTO Togrutetabell (TogruteID, Stasjonnummer, Stasjon, Ankomst, Avgang)
 VALUES
-('TrondheimSBodøMorgen', 1, 'Trondheim S', '00:00', '07:49'),
+('TrondheimSBodøMorgen', 1, 'Trondheim S', '07:49', '07:49'),
 ('TrondheimSBodøMorgen', 2, 'Steinkjer', '09:51', '09:51'),
 ('TrondheimSBodøMorgen', 3, 'Mosjøen', '13:20', '13:20'),
 ('TrondheimSBodøMorgen', 4, 'Mo i Rana', '14:31', '14:31'),
 ('TrondheimSBodøMorgen', 5, 'Fauske', '16:49', '16:49'),
-('TrondheimSBodøMorgen', 6, 'Bodø', '17:34', '00:00');
+('TrondheimSBodøMorgen', 6, 'Bodø', '17:34', '17:34');
 
 -- Togrute 2
 INSERT OR IGNORE INTO Togrute (TogruteID, Operatør, Togoppsett, Mandag, Tirsdag, Onsdag, Torsdag, Fredag, Lørdag, Søndag)
@@ -70,12 +70,12 @@ VALUES ('TrondheimSBodøNatt', 'SJ', 'EnSitteEnSove', TRUE, TRUE, TRUE, TRUE, TR
 
 INSERT OR IGNORE INTO Togrutetabell (TogruteID, Stasjonnummer, Stasjon, Ankomst, Avgang)
 VALUES
-('TrondheimSBodøNatt', 1, 'Trondheim S', '00:00', '23:05'),
+('TrondheimSBodøNatt', 1, 'Trondheim S', '23:05', '23:05'),
 ('TrondheimSBodøNatt', 2, 'Steinkjer', '00:57+1', '00:57+1'),
 ('TrondheimSBodøNatt', 3, 'Mosjøen', '04:41+1', '04:41+1'),
 ('TrondheimSBodøNatt', 4, 'Mo i Rana', '05:55+1', '05:55+1'),
 ('TrondheimSBodøNatt', 5, 'Fauske', '08:19+1', '08:19+1'),
-('TrondheimSBodøNatt', 6, 'Bodø', '09:05+1', '00:00');
+('TrondheimSBodøNatt', 6, 'Bodø', '09:05+1', '09:05+1');
 
 -- Togrute 3
 INSERT OR IGNORE INTO Togrute (TogruteID, Operatør, Togoppsett, Mandag, Tirsdag, Onsdag, Torsdag, Fredag, Lørdag, Søndag)
@@ -83,7 +83,7 @@ VALUES ('MoiRanaTrondheimSMorgen', 'SJ', 'EnSitte', TRUE, TRUE, TRUE, TRUE, TRUE
 
 INSERT OR IGNORE INTO Togrutetabell (TogruteID, Stasjonnummer, Stasjon, Ankomst, Avgang)
 VALUES
-('MoiRanaTrondheimSMorgen', 1, 'Mo i Rana', '00:00', '08:11'),
+('MoiRanaTrondheimSMorgen', 1, 'Mo i Rana', '08:11', '08:11'),
 ('MoiRanaTrondheimSMorgen', 2, 'Mosjøen', '09:14', '09:14'),
 ('MoiRanaTrondheimSMorgen', 3, 'Steinkjer', '12:31', '12:31'),
-('MoiRanaTrondheimSMorgen', 4, 'Trondheim S', '14:13', '00:00');
+('MoiRanaTrondheimSMorgen', 4, 'Trondheim S', '14:13', '14:13');
--- brukerhistorier/d.py
+++ brukerhistorier/d.py
@@ -5,8 +5,9 @@ import datetime
 import interface
 
 # d) Søk etter togruter mellom en startstasjon og en sluttstasjon på en gitt dato og tidspunkt.
-#    Resultatet inkludere reiser samme dag fra og med tidspunktet og reiser neste dag. 
+#    Resultatet inkludere reiser samme dag fra og med tidspunktet og reiser neste dag.
 #    Resultatet skal være sortert etter avgangstidspunkt.
+
 
 def search_togruter(conn: sqlite3.Connection, stdscr: curses.window):
     cursor = conn.cursor()
@@ -14,7 +15,8 @@ def search_togruter(conn: sqlite3.Connection, stdscr: curses.window):
     AnkomstStasjon = interface.input_stasjon(cursor, stdscr)
     Dato = interface.input_dato(cursor, stdscr)
     Tid = interface.input_klokkeslett(cursor, stdscr)
-    DatoPlusEnDag = (datetime.datetime.strptime(Dato, '%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    DatoPlusEnDag = (datetime.datetime.strptime(
+        Dato, '%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
     query = f"""
             SELECT tr.TogruteID, start.Stasjon AS AvreiseStasjon, 
@@ -26,34 +28,44 @@ def search_togruter(conn: sqlite3.Connection, stdscr: curses.window):
             JOIN Togruteforekomst tgf ON tr.TogruteID = tgf.Rute 
             WHERE LOWER(start.Stasjon) = LOWER(?) AND LOWER(slutt.Stasjon) = LOWER(?) 
                 AND (tgf.Togruteforekomstdato = ? OR tgf.Togruteforekomstdato = ?)
+                AND (start.avgang >= ? OR tgf.Togruteforekomstdato = ?)
             ORDER BY AvreiseDato, AvreiseTid ASC;
             """
-## Brukte denne DATEADD(day, 1, ?) etter And i nest siste linje.
+# Brukte denne DATEADD(day, 1, ?) etter And i nest siste linje.
 
-    params = (AvreiseStasjon, AnkomstStasjon, Dato, DatoPlusEnDag,)
+    params = (AvreiseStasjon, AnkomstStasjon, Dato,
+              DatoPlusEnDag, Tid, DatoPlusEnDag,)
     cursor.execute(query, params)
 
     ### Skriver resultattabellen i terminalen ###
     stdscr.clear()
-    rows = cursor.fetchall()
+    rows: list[str] = cursor.fetchall()
 
     if rows:
-        TogruteID_width      =  max(max(len(row[0]), len('TogruteID')) for row in rows) + 6
-        AvreiseStasjon_width =  max(max(len(row[1]), len('AvreiseStasjon'), len(AvreiseStasjon)) for row in rows) + 6
-        AvreiseDato_width    =  max(max(len(row[2]), len('AvreiseDato')) for row in rows) + 6
-        AvreiseTid_width     =  max(max(len(row[3]), len('AvreiseTid')) for row in rows) + 6
-        AnkomstStasjon_width =  max(max(len(row[4]), len('AnkomstStasjon'), len(AnkomstStasjon)) for row in rows) + 6
-#        AnkomstDato_width    =  max(max(len(row[5]), len('AnkomstDato')) for row in rows) + 6 skal egentlig v're row[5], men tar den vekk midlertidig
-        AnkomstTid_width     =  max(max(len(row[5]), len('AnkomstTid')) for row in rows) + 6
+        TogruteID_width = max(
+            max(len(row[0]), len('TogruteID')) for row in rows) + 6
+        AvreiseDato_width = max(
+            max(len(row[2]), len('AvreiseDato')) for row in rows) + 6
+        AvreiseTid_width = max(
+            max(len(row[3]), len('AvreiseTid')) for row in rows) + 6
+        AnkomstDato_width = max(
+            max(len(row[2]), len('Ankomstdato')) for row in rows) + 6
+        AnkomstTid_width = max(
+            max(len(row[5]), len('AnkomstTid')) for row in rows) + 6
 
-        header = f"{'TogruteID':<{TogruteID_width}}{'AvreiseStasjon':<{AvreiseStasjon_width}}{'AvreiseDato':<{AvreiseDato_width}}{'AvreiseTid':<{AvreiseTid_width}}{'AnkomstStasjon':<{AnkomstStasjon_width}}{'AnkomstTid':<{AnkomstTid_width}}"# {'AnkomstDato':<{AnkomstDato_width}} skal egentlig staa i den tomme linjen.
-        stdscr.addstr(0, 0, header, curses.color_pair(1) | curses.A_BOLD)
-        stdscr.addstr(1, 0, "-" * (len(header)))
+        header = f"{'TogruteID':<{TogruteID_width}}{'Avreisedato':<{AvreiseDato_width}}{'Avreisetid':<{AvreiseTid_width}}{'Ankomstdato':<{AnkomstDato_width}}{'Ankomsttid':<{AnkomstTid_width}}"
+        stdscr.addstr(0, 0, f"Togruter mellom {rows[0][1]} og {rows[0][4]}:", curses.color_pair(
+            2) | curses.A_BOLD)
+        stdscr.addstr(2, 0, header, curses.color_pair(1))
+        stdscr.addstr(3, 0, "-" * (len(header)))
 
         for idx, row in enumerate(rows):
-            result_row = f"{row[0]:<{TogruteID_width}}{row[1]:<{AvreiseStasjon_width}}{row[2]:<{AvreiseDato_width}}{row[3]:<{AvreiseTid_width}}{row[4]:<{AnkomstStasjon_width}}{row[5]:<{AnkomstTid_width}}"#{row[5]:<{AnkomstDato_width}} skal egentlig staa i den tomme linjen.
-            stdscr.addstr(idx + 3, 0, result_row, curses.color_pair(3))
+            AnkomstDato = (datetime.datetime.strptime(
+                row[2], '%Y-%m-%d') + datetime.timedelta(days=int(row[5].split("+")[1] if "+" in row[5] else 0))).strftime('%Y-%m-%d')
+            result_row = f"{row[0]:<{TogruteID_width}}{row[2]:<{AvreiseDato_width}}{row[3]:<{AvreiseTid_width}}{AnkomstDato:<{AnkomstDato_width}}{row[5].split('+')[0]:<{AnkomstTid_width}}"
+            stdscr.addstr(idx + 4, 0, result_row, curses.color_pair(3))
 
+        curses.curs_set(0)
         stdscr.refresh()
         stdscr.getch()
     else:
